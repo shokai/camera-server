718ae769e86018c7266b72450a9738f8a2c08444
Sho Hashimoto <hashimoto@shokai.org>
2011-06-15 01:46:56 +0900

    refact

--- viewer/img-server.js
+++ viewer/img-server.js
@@ -1,11 +1,16 @@
 var ImgServer = {};
-ImgServer.start = function(obj, url, delay_msec){
-    var img = $('<img>').attr('src',url+'?'+new Date().getTime());
+ImgServer.start = function(display_obj, img_url, delay_msec){
+    var img = $('<img>').attr('src',img_url+'?'+new Date().getTime());
+    var load_err_timer = setTimeout(function(){
+        console.log('load_err_timer');
+        ImgServer.start(display_obj, img_url, delay_msec);
+    }, 10000)
     img.load(function(){
-        obj.children('#view').html(img);
+        clearTimeout(load_err_timer);
+        display_obj.children('#view').html(img);
         setTimeout(function(){
-            ImgServer.start(obj, url, delay_msec);
+            ImgServer.start(display_obj, img_url, delay_msec);
         }, delay_msec);
     });
-    obj.children('#tmp').css('visibility','hidden').html(img);
+    display_obj.children('#tmp').css('visibility','hidden').html(img);
 };
--- viewer/main.js
+++
@@ -1,4 +0,0 @@
-
-$(function(){
-    ImgServer.start($('div#camera_img'), 'http://localhost:8080', 1000);
-});
