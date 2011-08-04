4621aa1a0f5fef84c1561703629205b7a70c6ab8
Sho Hashimoto <hashimoto@shokai.org>
2011-08-04 22:13:32 +0900

    class CameraServer on js

--- viewer/camera-server.js
+++ viewer/camera-server.js
@@ -1,17 +1,18 @@
-var CameraServer = {};
-CameraServer.count = 0;
+var CameraServer = function(){};
+CameraServer.prototype.count = 0;
 
-CameraServer.init = function(display_obj){
+CameraServer.prototype.init = function(display_obj){
     display_obj.append($('<span>').attr('id','0').hide());
     display_obj.append($('<span>').attr('id','1').hide());
 };
 
-CameraServer.do_reload = function(display_obj, img_url, delay_msec){
+CameraServer.prototype.do_reload = function(display_obj, img_url, delay_msec){
     var img = $('<img>').attr('src',img_url+'?'+new Date().getTime());
-    var id = CameraServer.count++ % 2;
+    var id = this.count++ % 2;
     var load_err_timer = setTimeout(function(){
-        CameraServer.do_reload(display_obj, img_url, delay_msec);
-    }, 10000)
+        this.do_reload(display_obj, img_url, delay_msec);
+    }, 10000);
+    var self = this;
     img.load(function(){
         clearTimeout(load_err_timer);
         display_obj.children('#'+id).show();
@@ -19,13 +20,13 @@ CameraServer.do_reload = function(display_obj, img_url, delay_msec){
         else id2 = 0;
         display_obj.children('#'+id2).hide();
         setTimeout(function(){
-            CameraServer.do_reload(display_obj, img_url, delay_msec);
+            self.do_reload(display_obj, img_url, delay_msec);
         }, delay_msec);
     });
     display_obj.children('#'+id).html(img);
 };
 
-CameraServer.start = function(display_obj, img_url, delay_msec){
-    CameraServer.init(display_obj);
-    CameraServer.do_reload(display_obj, img_url, delay_msec);
+CameraServer.prototype.start = function(display_obj, img_url, delay_msec){
+    this.init(display_obj);
+    this.do_reload(display_obj, img_url, delay_msec);
 };
